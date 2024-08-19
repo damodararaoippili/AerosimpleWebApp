9d6d7e384d8036a3bef17c30b0fe6c0b02112965
damodarippili <damodarippili@gmail.com>
2024-08-19 22:31:48 +0530

    Browser Invoking

--- BrowserInvoke.py
+++ BrowserInvoke.py
@@ -1,21 +1,17 @@
-import time
 import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from LoginPage import LoginPage
 
 class TestLogin(unittest.TestCase):
     def setUp(self):
-        # Set up ChromeDriver with webdriver-manager
+        # Set up the ChromeDriver
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.driver.maximize_window()
-        self.login_page = LoginPage(self.driver)
 
     def test_successful_login(self):
-        self.login_page.perform_login()
-        time.sleep(20)
-        # Add assertions to verify successful login, e.g., check for a welcome message or redirect URL
+        self.driver.get("https://us.staging.aerosimple.com/")
+        # Add your test code here
 
     def tearDown(self):
         self.driver.quit()
